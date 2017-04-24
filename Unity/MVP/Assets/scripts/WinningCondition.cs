1e33e569eaa82c1e2076a0b044b634096de41ae4
Maia Cordero <mcordero@isointeractive.com>
2017-04-24 06:48:43 -0300

    add ui menu and game refactor prefabs with new features

--- Unity/MVP/Assets/scripts/WinningCondition.cs
+++ Unity/MVP/Assets/scripts/WinningCondition.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WinningCondition : MonoBehaviour {
-	public GameObject triggerWin;
-    public GameObject fadeOutObj;
-    public GameObject playerController;
+public class WinningCondition : MonoBehaviour
+{
+    public ApplicationManager appManager;
+
+    //public GameController gameController;
+    //public GameObject triggerWin;
+    //public GameObject fadeOutObj;
+   // public GameObject playerController;
 
     void OnTriggerEnter(Collider col)
     {
-        print("Ganaste");
-        fadeOutObj.SetActive(true);
-        playerController.SetActive(false);
+        appManager.EndGame();
+
+        //print("Ganaste");
+        //fadeOutObj.SetActive(true);
+        //playerController.SetActive(false);
     }
 
     void FadingOut()
