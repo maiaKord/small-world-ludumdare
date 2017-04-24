556bdc93ecfdc4f39352338b7d5a18c5c8217293
Ismael Iszlonn <iszlonn@gmail.com>
2017-04-24 04:04:24 -0300

    Added stars, reduced particle size, added UV scrolling (changes wall color), WIn condition with a "FadeOut" object that activates over the camera.

---
+++ Unity/MVP/Assets/scripts/StarsMov.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarsMov : MonoBehaviour {
+    public float movSpeed;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, movSpeed, 0);
+	}
+}
---
+++ Unity/MVP/Assets/scripts/WinningCondition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinningCondition : MonoBehaviour {
+	public GameObject triggerWin;
+    public GameObject fadeOutObj;
+    public GameObject playerController;
+
+    void OnTriggerEnter(Collider col)
+    {
+        print("Ganaste");
+        fadeOutObj.SetActive(true);
+        playerController.SetActive(false);
+    }
+
+    void FadingOut()
+    {
+
+       
+    }
+}
